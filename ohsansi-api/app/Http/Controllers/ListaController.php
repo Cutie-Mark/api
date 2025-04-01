5d9c37b237208a5eda0cb1f16303d5932765ad29
PHansLM <pablito.hans@hotmail.com>
2025-04-01 00:41:41 -0400

    feature / migraciones lista

---
+++ ohsansi-api/app/Http/Controllers/ListaController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lista;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class ListaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Lista::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'nombre_lista' => 'required|string|max:255',
+            ]);
+
+            $lista = Lista::create($validatedData);
+
+            return response()->json(['message' => 'Lista creada con éxito', 'lista' => $lista], 201);
+        } catch (ValidationException $e) {
+            return response()->json(['errors' => $e->errors()], 422);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $lista = Lista::find($id);
+        if (!$lista) {
+            return response()->json(['message' => 'Lista no encontrada'], 404);
+        }
+        return response()->json($lista);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $lista = Lista::findOrFail($id);
+
+        $request->validate([
+            'nombre_lista' => 'sometimes|string|max:255',
+        ]);
+
+        $lista->update($request->only('nombre_lista'));
+
+        return response()->json($lista);
+    }
+}
---
+++ ohsansi-api/app/Models/Lista.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lista extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nombre_lista', 'codigo_lista'];
+
+    public $timestamps = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($lista) {
+            $lista->codigo_lista = strtoupper(bin2hex(random_bytes(8))); // Genera 16 caracteres alfanuméricos
+        });
+    }
+}
