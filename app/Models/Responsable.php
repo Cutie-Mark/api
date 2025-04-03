ddb5527fc6be02a7fecb32b6c12b2169522859ad
PHansLM <pablito.hans@hotmail.com>
2025-04-02 20:32:19 -0400

    ajuste de relacion de listas

--- app/Models/Responsable.php
+++ app/Models/Responsable.php
@@ -10,4 +10,9 @@ class Responsable extends Model
     use HasFactory;
 
     protected $fillable = ['nombre', 'apellido', 'email', 'telefono'];
+
+    public function responsables()
+    {
+        return $this->hasMany(Lista::class);
+    }
 }
