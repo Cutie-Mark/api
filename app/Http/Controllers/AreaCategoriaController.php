df3160f4e8dbfa342626e4b83aa3e5527d8221c2
PHansLM <pablito.hans@hotmail.com>
2025-04-06 19:57:32 -0400

    ajuste / salidas de areas y categorias completas

--- app/Http/Controllers/AreaCategoriaController.php
+++ app/Http/Controllers/AreaCategoriaController.php
@@ -30,24 +30,14 @@ class AreaCategoriaController extends Controller
     // 2. Obtener las áreas con sus categorías 
     public function getAllAreasWithCategorias()
     {
-        $areas = Area::with(['categorias:id'])->get();
-
-        $areas->each(function ($area) {
-            $area->categorias = $area->categorias->pluck('id');
-        });
-
+        $areas = Area::with('categorias')->get();
         return response()->json($areas);
     }
 
     // 3. Obtener las categorías con sus áreas
     public function getAllCategoriasWithAreas()
     {
-        $categorias = Categoria::with(['areas:id'])->get();
-
-        $categorias->each(function ($categoria) {
-            $categoria->areas = $categoria->areas->pluck('id');
-        });
-
+        $categorias = Categoria::with('areas')->get();
         return response()->json($categorias);
     }
 
