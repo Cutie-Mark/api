5a54da0e64ef4faaf8eb7757b86bd304c974e3b0
PHansLM <pablito.hans@hotmail.com>
2025-04-06 11:49:34 -0400

    inicio de sesion basico

---
+++ database/seeders/UsuarioSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Usuario;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Usuario::create([
+            'nombre_usuario' => 'admin',
+            'password' => Hash::make('admin'),
+        ]);
+    }
+}
