2a26475d2e0902033f30b59f9e1ef585fbfd2530
PHansLM <pablito.hans@hotmail.com>
2025-05-08 17:09:30 -0400

    extra / seeder de olimpiadas con nuevos campos

--- database/seeders/OlimpiadaSeeder.php
+++ database/seeders/OlimpiadaSeeder.php
@@ -21,6 +21,9 @@ class OlimpiadaSeeder extends Seeder
             'gestion' => '2025 - I',
             'fecha_inicio' => Carbon::create('2025', '04', '15'),
             'fecha_fin' => Carbon::create('2025', '05', '20'),
+            'precio_inscripcion' => 16,
+            'limite_inscripciones' => 2,
+            'descripcion_convocatoria' => ' .'
         ]);
 
         Olimpiada::create([
@@ -28,6 +31,9 @@ class OlimpiadaSeeder extends Seeder
             'gestion' => '2025 - II',
             'fecha_inicio' => Carbon::create('2025', '07', '15'),
             'fecha_fin' => Carbon::create('2025', '08', '20'),
+            'precio_inscripcion' => 16,
+            'limite_inscripciones' => 2,
+            'descripcion_convocatoria' => '.'
         ]);
 
         Olimpiada::create([
@@ -35,6 +41,9 @@ class OlimpiadaSeeder extends Seeder
             'gestion' => '2026 - I',
             'fecha_inicio' => Carbon::create('2026', '04', '10'),
             'fecha_fin' => Carbon::create('2026', '05', '15'),
+            'precio_inscripcion' => 16,
+            'limite_inscripciones' => 2,
+            'descripcion_convocatoria' => ' .'
         ]);
     }
 }
