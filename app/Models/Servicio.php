a7331100202bba36e081ae36ad94adce3707683b
PHansLM <pablito.hans@hotmail.com>
2025-05-10 23:22:22 -0400

    ajuste / registro de roles ajustado

--- app/Models/Servicio.php
+++ app/Models/Servicio.php
@@ -9,6 +9,8 @@ class Servicio extends Model
 {
     protected $fillable = ['nombre', 'descripcion'];
 
+    protected $hidden = ['created_at', 'updated_at'];
+
     public function roles()
     {
         return $this->belongsToMany(Rol::class, 'servicio_rol');
--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Rol;
+
 
 class Usuario extends Authenticatable
 {
@@ -32,7 +34,7 @@ class Usuario extends Authenticatable
 
     public function roles()
     {
-        return $this->belongsToMany(Rol::class, 'rol_usuario');
+        return $this->belongsToMany(Rol::class, 'rol_usuario', 'usuario_id', 'rol_id');
     }
 
     public function checkAcceso(string $servicioNombre): bool
