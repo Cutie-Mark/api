2f2d8c56994d0a1b26fe90439cd9ab93a3ea2e2f
jHOkE <jhonumss@gmail.com>
2025-06-02 19:46:58 -0400

    activando

--- routes/api.php
+++ routes/api.php
@@ -83,7 +83,7 @@ Route::prefix('/cronogramas')->group(function () {
 // =========================
 Route::prefix('/olimpiadas')->group(function () {
     Route::get('/', [OlimpiadaController::class, 'index']);                                         // Obtener todas las olimpiadas
-    //-Route::post('/por-fases', [OlimpiadaController::class, 'getOlimpiadasByFases']);                // Obtener olimpiadas por fases jhon lo utiliza xd
+    Route::post('/por-fases', [OlimpiadaController::class, 'getOlimpiadasByFases']);                // Obtener olimpiadas por fases jhon lo utiliza xd
     Route::get('/pasadas', [OlimpiadaController::class, 'getOlimpiadasPasadas']);                   // Obtener olimpiadas pasadas
     Route::get('/futuras', [OlimpiadaController::class, 'getOlimpiadasFuturas']);
     Route::get('/hoy', [OlimpiadaController::class, 'checkOlimpiadaEnCurso']);                      // Consultar si hay olimpiada en curso
